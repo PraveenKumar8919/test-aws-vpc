941e13b8c3d96ae3fadf9740a0adea15b2d975b3
PraveenKumar8919 <ppattiri@aum.edu>
2024-06-24 22:12:30 -0500

    Terraform module

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "roboshop" {
     source = "../terraform-aws-vpc"
+    #source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
@@ -13,4 +14,7 @@ module "roboshop" {
 
   # private subnet
   database_subnets_cidr = var.database_subnets_cidr
+
+  #peering
+  is_peering_required = var.is_peering_required
 }
--- variables.tf
+++ variables.tf
@@ -35,6 +35,11 @@ variable "private_subnets_cidr" {
   default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
-variable "private_subnets_cidr" {
+variable "database_subnets_cidr" {
   default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "is_peering_required" {
+  default = true
+  
 }
